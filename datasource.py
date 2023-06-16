989b6517932d279eb9ca204b03126cdf7375d554
wawahuang <wawahuang321@gmail.com>
2023-06-16 10:22:09 +0800

    modify

---
+++ datasource.py
@@ -0,0 +1,39 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+def create_table(conn):
+    sql_projects = """
+    CREATE TABLE IF NOT EXISTS led(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		date TEXT NOT NULL,
+		state INTEGER NOT NULL		
+    );
+    """
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute(sql_projects)
+        print("Table created successfully")  
+    except Error as e:
+        print(e)
+
+def insert_data(state):
+    conn = create_connection('iot.db')
+    create_table(conn)
+
+    sql_insert = """
+    INSERT INTO led(date, state) VALUES(datetime('now','localtime'), ?);
+    """
+    cursor = conn.cursor()
+    cursor.execute(sql_insert, (state,))
+    conn.commit()
+    conn.close()
